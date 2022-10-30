4e650eed2be84b225571c90b141852ad6e6c13f7
v3locity47 <aryanmishra530@gmail.com>
2022-10-31 00:14:45 +0530

    Adds Mongo Config

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,1 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
+
+const connectDB = async () => {
+  const mongoConnection = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as ConnectOptions);
+
+  if (mongoConnection) console.log("MongoDB Connected");
+};
+
+export { connectDB };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express, { Request, Response } from "express";
+import { connectDB } from "./config/db";
 import { createServer } from "http";
 
 const app = express();
 app.use(express.json);
 
 const httpServer = createServer(app);
+connectDB();
 httpServer.listen(8082, () => {
   console.log(`Server Running on Port: ${process.env.PORT}`);
 });
